5cb6cc79e70f472d8e9b3d1916c89984bd836c14
Farzam Afzal <rj412169@dal.ca>
2024-07-04 22:27:14 -0500

    data exploration continued

--- fraud-detection-analysis.R
+++ fraud-detection-analysis.R
@@ -13,4 +13,9 @@ library(data.table)
 creditcard_data <- read.csv("D:\\Credit Card Fraud Detection Project\\creditcard.csv")
 
 #DATA EXPLORATION SECTION
-dim(creditcard_data)
+dim(creditcard_data) #checks the dimensions of the dataset
+head(creditcard_data, 6) #views the first 6 rows of dataset
+tail(creditcard_data, 6) #views the last 6 rows of dataset
+
+# Counts the number of fraudulent and non-fraudulent transactions
+table(creditcard_data$Class) 
